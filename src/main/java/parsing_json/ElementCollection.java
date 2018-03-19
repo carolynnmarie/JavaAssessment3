7d567be96072e9a177440abcd0869c69bf753623
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-03-19 15:20:59 -0400

    I am an inch away from giving up and going home so I should commit the tiny bit I completed in case I decide to do so.  I think it is deeply unfair to have the entire test not on things but on how fast we can learn something totally and completely new.

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,16 +1,42 @@
 package parsing_json;
 
-public class ElementCollection {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
 
-    public Element findByAtomicNumber(int atomic_number) {
-        return null;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+
+
+public class ElementCollection extends ArrayList {
+
+
+    public Element findByAtomicNumber(int atomic_number) throws IOException {
+
+        Gson gson = new Gson();
+
+
     }
-
     public Element findByName(String name) {
         return null;
     }
 
-    public ElementCollection where(String fieldName, Object value) {
+    public ElementCollection where(String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
+//        ElementCollection =
+//        Element el = new Element();
+//        Field f = el.getClass().getField(fieldName);
+//        value = f.get(el);
+
+
+
+
+
+
         return null;
     }
 }
