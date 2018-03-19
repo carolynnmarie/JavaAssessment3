41f6a50a186b3a0610bdecfce5aeea3242b767dc
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-03-19 16:17:50 -0400

    final commit.  I give up.

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,5 +1,4 @@
 package parsing_json;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,29 +12,28 @@ import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 
 
-public class ElementCollection extends ArrayList {
-
+public class ElementCollection extends ArrayList<Element> {
 
     public Element findByAtomicNumber(int atomic_number) throws IOException {
-
         Gson gson = new Gson();
-
-
+        String x = Element.class.getResource("/periodic_table.json").getFile();
+        JsonParser parser = new JsonParser();
+        JsonArray jsonArray = parser.parse(x).getAsJsonArray();
+        ArrayList<Element> al = gson.fromJson(jsonArray, ArrayList.class);
+        Element element = new Element();
+        for (int i = 0; i < al.size(); i++) {
+            if (al.get(i).getNumber() == atomic_number) {
+                element = al.get(i);
+            }
+        }
+        return element;
     }
+
     public Element findByName(String name) {
         return null;
     }
 
     public ElementCollection where(String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
-//        ElementCollection =
-//        Element el = new Element();
-//        Field f = el.getClass().getField(fieldName);
-//        value = f.get(el);
-
-
-
-
-
 
         return null;
     }
